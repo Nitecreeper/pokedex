5d0ce53ec35c88fd9c80624b8039cb5632700f85
Jorge Guepud <jlguepudb@unal.edu.co>
2023-10-02 17:19:26 -0500

    :sparkles: feat: Se agrega la tarjeta para cada pokemon

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { PokemonListComponent } from "./pages/pokemon-list/pokemon-list.component";
 
 const routes: Routes = [
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { PokemonRoutingModule } from './pokemon-routing.module';
+
 import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 
 
 @NgModule({
   declarations: [
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PokemonRoutingModule
   ]
 })
 export class PokemonModule { }
