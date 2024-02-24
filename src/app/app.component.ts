5ec4ca1c80ffb197142c2d81d61978046b762caa
Jorge Guepud <jlguepudb@unal.edu.co>
2024-02-24 10:11:01 -0500

    feat: se agrega la lógica del service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponentsModule } from './components/components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ComponentsModule,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/pokemon-api/pokemon-api.service.ts
+++ src/app/services/pokemon-api/pokemon-api.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { PokemonList } from '../../interfaces/pokemon-list-interfaces';
+import { Pokemon } from '../../interfaces/pokemon.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonApiService {
 
-  constructor() { }
+  private apiPath: string = 'https://pokeapi.co/api/v2/';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public pokemonList(limit: number, offset: number): Observable<PokemonList>{
+
+    
+    const url = `${this.apiPath}pokemon?limit=${limit}&offset=${offset}`;
+    
+    console.log('::: PokemonService: pokemonList ::: ->','Get a la url:', url)
+    
+    return this.httpClient.get<PokemonList>(url)
+                .pipe( catchError( () => of()) );
+  }
+
+  public pokemonData(pokemonUrl: string): Observable<Pokemon>{
+    return this.httpClient.get<Pokemon>(pokemonUrl)
+                .pipe( catchError( () => of()) );
+  }
 }
